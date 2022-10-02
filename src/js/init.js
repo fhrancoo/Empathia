43075d0a526aa5ce8c963769679dacc05205a7c5
fhrancoo <132franco@gmail.com>
2022-10-02 19:39:00 -0300

    new update"

--- src/js/init.js
+++ src/js/init.js
@@ -6,14 +6,18 @@ windowResize()
 positionNav();
 $('.sidenav').sidenav();
 $('.parallax').parallax();
+$('.carousel').carousel({ shift: 20, padding: 60});
 
 //after
 $(window).resize(function () {
   heightNav = $('nav').outerHeight(true);
   heightWindow = $(window).height();
-  $('.slider').slider({ indicators: false, height: heightWindow });
   
-  console.log('ASDASDASD')
+  if ($(window).width() <= 600) {
+    $("#banner").attr("src","./src/img/logo-vertical.jpg");
+  } else $("#banner").attr("src","./src/img/Empathia-slogan.png");
+
+  $('.slider').slider({ indicators: false, height: heightWindow });
 });
 
 $(window).scroll(function () {
@@ -23,6 +27,10 @@ $(window).scroll(function () {
 function windowResize() {
   heightNav = $('nav').outerHeight(true);
   heightWindow = $(window).height();
+
+  if ($(window).width() <= 600) {
+    $("#banner").attr("src","./src/img/logo-vertical.jpg");
+  } else $("#banner").attr("src","./src/img/Empathia-slogan.png");
 
   $('.slider').slider({ indicators: false, height: heightWindow });
 }
