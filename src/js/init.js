29e82338365f03bf891feec632d0e2d56d95e40c
fhrancoo <132franco@gmail.com>
2022-10-28 00:56:03 -0300

    update banner

--- src/js/init.js
+++ src/js/init.js
@@ -18,7 +18,7 @@ $(window).resize(function () {
   
   if ($(window).width() <= 600) {
     $("#banner").attr("src", "./src/img/logo-vertical.jpg");
-    $("#banner2").attr("src","./src/img/banner2.jpg");
+    $("#banner2").attr("src","./src/img/banner2-phone.jpg");
   } else {
     $("#banner").attr("src", "./src/img/Empathia-slogan.png");
     $("#banner2").attr("src","./src/img/banner2.jpg");
@@ -37,7 +37,7 @@ function windowResize() {
 
   if ($(window).width() <= 600) {
     $("#banner").attr("src", "./src/img/logo-vertical.jpg");
-    $("#banner2").attr("src","./src/img/banner2.jpg");
+    $("#banner2").attr("src","./src/img/banner2-phone.jpg");
   } else {
     $("#banner").attr("src", "./src/img/Empathia-slogan.png");
     $("#banner2").attr("src","./src/img/banner2.jpg");
