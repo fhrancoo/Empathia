7700b129e3f359170f6f981baeca8c2b51cbfcb7
fhrancoo <132franco@gmail.com>
2022-11-01 19:35:06 -0300

    removed tooltips

--- src/js/init.js
+++ src/js/init.js
@@ -1,17 +1,19 @@
-
+//Init
 let heightNav;
 let heightWindow;
 
 windowResize()
 positionNav();
 $('.parallax').parallax();
-$('.tooltipped').tooltip();
 $('.modal').modal();
 const sidenav = $('.sidenav').sidenav();
 const carousel = $('.carousel').carousel();
 const tabs = $('.tabs').tabs();
+const instanceTabs = M.Tabs.getInstance(tabs);
+const instanceSidenav = M.Sidenav.getInstance(sidenav);
 
-//after
+
+//Functions
 $(window).resize(function () {
   heightNav = $('nav').outerHeight(true);
   heightWindow = $(window).height();
@@ -41,7 +43,6 @@ function windowResize() {
   } else {
     $("#banner").attr("src", "./src/img/Empathia-slogan.png");
     $("#banner2").attr("src","./src/img/banner2.jpg");
-
   }
 
   $('.slider').slider({ indicators: false, height: heightWindow });
@@ -57,5 +58,44 @@ function positionNav() {
   } else {
     $('nav').removeClass('fixed white').addClass('shadowDelete');
     $('nav ul a').addClass('hoverDelete');
+  }
+}
+
+function redes(params) {
+  switch (params) {
+      case "ig":
+        window.open("https://www.instagram.com/empathia.agencia/", "_blank");
+      break;
+      case "fb":
+        window.open("https://www.facebook.com/profile.php?id=100086478524870","_blank");
+      break;
+    case "tw":
+      window.open("https://twitter.com/EmpathiaAgencia","_blank");
+      break;
+    case "ln":
+      break;
+    default:
+      break;
+  }
+}
+
+function pageTabs(params) {
+  switch (params) {
+    case "home":
+      instanceTabs.select('home')
+      instanceSidenav.close();
+      break;
+    case "nosotros":
+      instanceTabs.select('nosotros')
+      instanceSidenav.close();
+      break;
+    case "servis":
+      instanceTabs.select('servis')
+      instanceSidenav.close();
+      break;
+    default:
+      instanceTabs.select('home')
+      instanceSidenav.close();
+      break;
   }
 }
